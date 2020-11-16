f5d3214a413f732c53fbd65b8dad97c00c36d093
puglisac <alan.c.puglisi@gmail.com>
2020-11-16 11:58:32 -0600

    using conditionals

---
+++ UnitTestingProject/FlowControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace UnitTestingProject
+{
+    public class FlowControl
+    {
+        public bool IsYourFavoriteColorRed(string color)
+        {
+            if (color.ToLower() == "blue")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool IsYourFavoriteColorBlue(string color)
+        {
+            if (color.ToLower() == "blue")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public FlowControl()
+        {
+        }
+    }
+}
