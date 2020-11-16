3ec72e964afb19750bd3355d89f1c4ff56fda2dd
puglisac <alan.c.puglisi@gmail.com>
2020-11-16 12:42:44 -0600

    added more conditionals and unit testing for flowcontrol

---
+++ Tests/FlowControlTest.cs
@@ -0,0 +1,85 @@
+using System;
+using Xunit;
+using UnitTestingProject;
+
+namespace Tests
+{
+    public class FlowControlTest
+    {
+        private FlowControl CreateFlowControl()
+        {
+            return new FlowControl();
+        }
+
+        [Fact]
+        public void FavoriteBlueTest()
+        {
+            var testInstance = CreateFlowControl();
+            Assert.True(testInstance.IsYourFavoriteColorBlue("blue"));
+            Assert.False(testInstance.IsYourFavoriteColorBlue("green"));
+
+        }
+        [Fact]
+        public void FavoriteRedTest()
+        {
+            var testInstance = CreateFlowControl();
+            Assert.True(testInstance.IsYourFavoriteColorRed("red"));
+            Assert.False(testInstance.IsYourFavoriteColorRed("green"));
+
+        }
+        [Fact]
+        public void FavoriteGreenTest()
+        {
+            var testInstance = CreateFlowControl();
+            Assert.True(testInstance.IsYourFavoriteColorGreen("green"));
+            Assert.False(testInstance.IsYourFavoriteColorGreen("yellow"));
+
+        }
+        [Fact]
+        public void FavoriteYellowTest()
+        {
+            var testInstance = CreateFlowControl();
+            Assert.True(testInstance.IsYourFavoriteColorYellow("yellow"));
+            Assert.False(testInstance.IsYourFavoriteColorYellow("green"));
+
+        }
+        [Fact]
+        public void PrimaryOrSecondaryTest()
+        {
+            var testInstance = CreateFlowControl();
+            Assert.Equal("Primary", testInstance.PrimaryOrSecondary("blue"));
+            Assert.Equal("Secondary", testInstance.PrimaryOrSecondary("green"));
+
+        }
+        [Fact]
+        public void PrimaryOrSecondarySwitchTest()
+        {
+            var testInstance = CreateFlowControl();
+            Assert.Equal("Primary", testInstance.PrimaryOrSecondarySwitch("blue"));
+            Assert.Equal("Secondary", testInstance.PrimaryOrSecondarySwitch("green"));
+
+        }
+        [Fact]
+        public void PrimaryOrSecondaryCompoundTest()
+        {
+            var testInstance = CreateFlowControl();
+            Assert.Equal("Primary", testInstance.PrimaryOrSecondaryCompound("blue"));
+            Assert.Equal("Secondary", testInstance.PrimaryOrSecondaryCompound("green"));
+
+        }
+        [Fact]
+        public void GradeLetterTest()
+        {
+            var testInstance = CreateFlowControl();
+            Assert.Equal("C", testInstance.GradeLetter(70));
+            Assert.Equal("F", testInstance.GradeLetter(50));
+        }
+        [Fact]
+        public void IsYourNameNotBruceTest()
+        {
+            var testInstance = CreateFlowControl();
+            Assert.StartsWith("Can we call", testInstance.IsYourNameNotBruce("NotBruce"));
+            Assert.StartsWith("G'day", testInstance.IsYourNameNotBruce("Bruce"));
+        }
+    }
+}
--- UnitTestingProject/FlowControl.cs
+++ UnitTestingProject/FlowControl.cs
@@ -1,19 +1,104 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace UnitTestingProject
 {
     public class FlowControl
+    
     {
-        public bool IsYourFavoriteColorRed(string color)
+
+        public string IsYourNameNotBruce(string yourName)
         {
-            if (color.ToLower() == "blue")
+            if(yourName.ToLower()!= "bruce")
             {
-                return true;
+                return "Can we call you Bruce for the sake of simplicity?";
             }
             else
             {
-                return false;
+                return "G'day Bruce";
             }
         }
+
+        //using &&
+        public string GradeLetter(int score)
+        {
+            if (score > 89) return "A";
+            else if (score > 79 && score < 90) return "B";
+            else if (score > 69 && score < 80) return "C";
+            else return "F";
+        }
+
+        //if, else, else if
+        public string PrimaryOrSecondary(string color)
+        {
+            var result = "";
+            if (color.ToLower() == "red")
+            {
+                result = "Primary";
+            }
+            else if (color.ToLower() == "blue")
+            {
+                result = "Primary";
+            }
+            else if (color.ToLower() == "yellow")
+            {
+                result = "Primary";
+            }
+            else
+            {
+                result = "Secondary";
+            }
+            return result;
+        }
+
+        //same as above but using switch statement
+        public string PrimaryOrSecondarySwitch(String color)
+        {
+            var result = "";
+            switch (color.ToLower())
+            {
+                case "red":
+                    result = "Primary";
+                    //could have more statements here
+                    break;
+                case "blue":
+                    result = "Primary";
+                    break;
+                case "yello":
+                    result = "Primary";
+                    break;
+                default:
+                    result = "Secondary";
+                    break;
+            }
+            return result;
+        }
+
+        //using or within conditional
+        public string PrimaryOrSecondaryCompound(string color)
+        {
+            var input = color.ToLower();
+            if (input == "red" || input == "blue" || input == "yellow") return "Primary";
+            return "Secondary";
+        }
+
+        //different ways of using conditionals
+        public bool IsYourFavoriteColorYellow(string color)
+        {
+            return color.ToLower() == "yellow";
+        }
+
+        public bool IsYourFavoriteColorGreen(string color)
+        {
+            return (color.ToLower() == "green") ? true : false;
+        }
+
+        public bool IsYourFavoriteColorRed(string color)
+        {
+            if (color.ToLower() == "red") return true;
+            return false;        
+        }
+
         public bool IsYourFavoriteColorBlue(string color)
         {
             if (color.ToLower() == "blue")
