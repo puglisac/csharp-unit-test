b426270cba87a3024618b5e370c2176559b94798
puglisac <alan.c.puglisi@gmail.com>
2020-11-16 11:31:08 -0600

    added awesomesauce list and test

---
+++ Tests/AwesomeSauceTest.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using UnitTestingProject;
+
+namespace Tests
+{
+    public class AwesomeSauceTest
+    {
+        [Fact]
+        public void IsSauceAwesomeTest()
+        {
+            var testInstance = new AwesomeSauce();
+            testInstance.Sauces.Add("Tobasco");
+            testInstance.Sauces.Add("Cholula");
+            testInstance.Sauces.Add("Trailer Trash");
+
+            Assert.True(testInstance.isSauceAwesome("Cholula"));
+            Assert.False(testInstance.isSauceAwesome("Ketchup"));
+
+        }
+    }
+}
